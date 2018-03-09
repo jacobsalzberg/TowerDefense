dfdd5d036d8c3fbef6e53bc9155763e825b3adcd
Jacob Salzberg <jacob1818@gmail.com>
2018-03-09 19:36:53 -0300

    Edit collision boxes for projectile and Add getters to projectile types

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ProType
+{
+    rock, arrow, fireball
+};
+
+
+
+
+public class Projectile : MonoBehaviour {
+
+    //each projectile is going to know its own attack strength
+    [SerializeField]
+    private int attackStrength;
+    [SerializeField]
+    private ProType projectileType;
+
+    //Getters
+
+    public int AttackStrength
+    {
+        get {
+            return attackStrength;
+        }        
+    }
+
+    public ProType ProjectileType
+    {
+        get
+        {
+            return projectileType;
+        }
+    }
+}
