251d02340573aa23e9d9263e55eb414c84aff884
Jacob Salzberg <jacob1818@gmail.com>
2018-03-09 02:29:13 -0300

    Add TowerPlacement system, add raycasts, add 2d collisions

---
+++ Assets/Scripts/TowerManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TowerManager : Singleton<TowerManager> {
+
+    private TowerBtn towerBtnPresed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(worldPoint,Vector2.zero);
+
+            PlaceTower(hit);
+        }
+	}
+
+    public void PlaceTower (RaycastHit2D hit)
+    {
+        if (!EventSystem.current.IsPointerOverGameObject() && towerBtnPresed !=null )
+        {
+            GameObject newTower = Instantiate(towerBtnPresed.TowerObject);
+            newTower.transform.position = hit.transform.position;
+        }
+    }
+    public void SelectedTower(TowerBtn towerSelected)
+    {
+        towerBtnPresed = towerSelected;
+    }
+}
