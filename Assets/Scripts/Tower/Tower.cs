2d6507b000f30cf0f6cc59ebb08eb5c178c9811e
Jacob Salzberg <jacob1818@gmail.com>
2018-03-10 01:13:16 -0300

    Code enemydistance routine

--- Assets/Scripts/Tower/Tower.cs
+++ Assets/Scripts/Tower/Tower.cs
@@ -27,13 +27,25 @@ public class Tower : MonoBehaviour {
         List<Enemy> enemiesInRange = new List<Enemy>();
         foreach(Enemy enemy in GameManager.Instance.EnemyList)
         {
-
+            if (Vector2.Distance(transform.position, enemy.transform.position) <= attackRadius){
+                enemiesInRange.Add(enemy);
+            }
         }
         return enemiesInRange;
     }
 
     private Enemy GetNearestEnemyInRange()
     {
-        
+        Enemy nearestEnemy = null;
+        float smallestDistance = float.PositiveInfinity; // comeca do maior valor
+        foreach (Enemy enemy in GetEnemiesInRange())
+        {
+            if (Vector2.Distance(transform.position, enemy.transform.position) <smallestDistance)
+            {
+                smallestDistance = Vector2.Distance(transform.position, enemy.transform.position);
+                nearestEnemy = enemy;
+            }
+        }
+        return nearestEnemy;
     }
 }
