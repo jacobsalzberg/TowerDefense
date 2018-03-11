c5bc40796c5a468a3a8df03c708ed58951d76130
Jacob Salzberg <jacob1818@gmail.com>
2018-03-10 23:04:10 -0300

    Add towerlist and collisionlist

--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -7,11 +7,16 @@ public class TowerManager : Singleton<TowerManager> {
 
     public TowerBtn towerBtnPresed { get; set; }
     private SpriteRenderer spriteRenderer;
+    private List<Tower> TowerList = new List<Tower>();
+    // list of build tiles!! remember what coliders we named
+    private List<Collider2D> BuildList = new List<Collider2D>();
+    private Collider2D buildTile;
 
 	// Use this for initialization
 	void Start () {
         //going to look for spriterender in the towermanager
-        spriteRenderer = GetComponent<SpriteRenderer>(); 
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        buildTile = GetComponent<Collider2D>();
 	}
 	
 	// Update is called once per frame
@@ -32,6 +37,34 @@ public class TowerManager : Singleton<TowerManager> {
         {
             FollowMouse();
         }
+    }
+    
+    public void RegisterBuildSite(Collider2D buildTag)
+    {
+        BuildList.Add(buildTag);
+    }
+
+    public void RegisterTower(Tower tower)
+    {
+        TowerList.Add(tower);
+    }
+
+    public void RenameTagsBuildSite()
+    {
+        foreach(Collider2D buildTag in BuildList)
+        {
+            buildTag.tag = "BuildSite";
+        }
+        BuildList.Clear();
+    }
+
+    public void DestroyAllTower()
+    {
+        foreach(Tower tower in TowerList)
+        {
+            Destroy(tower.gameObject);
+        }
+        TowerList.Clear();
     }
 
     public void PlaceTower (RaycastHit2D hit)
@@ -77,5 +110,6 @@ public class TowerManager : Singleton<TowerManager> {
     public void DisableDragSprite()
     {
         spriteRenderer.enabled = false;
+        //towerBtnPresed = null;
     }
 }
